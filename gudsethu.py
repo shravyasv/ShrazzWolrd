add47320d968b3202a71c8002873f5beae290870
shravyasv <sshaaru70@gmail.com>
2026-07-17 14:19:14 +0530

    Update gudsethu.py

--- gudsethu.py
+++ gudsethu.py
@@ -14,7 +14,6 @@ while True:
         choice = input("Enter choice: ")
 
         if choice == "1":
-            Catogary1 = "PG"
             PGN = input("Name of PG: ")
             Type1 = input("Type of PG: ")
             Rent_Per_Month1 = float(input("Rent Per Month: "))
@@ -25,10 +24,11 @@ while True:
             Address1 = input("Address of PG: ")
             Contact_No1 =input("For More Information Contact to This: ")
       
-            details.append([ Catogary1,PGN,Type1,Rent_Per_Month1,Deposite1,Time1,Food_Menu1,Name1,Address1,Contact_No1])    
+            details.append({"Category":"PG","Name":PGN,"Type":Type1,"Rent":Rent_Per_Month1,
+                            "Deposite":Deposite1,"Time":Time1,"Food":Food_Menu1,"Owner":Name1,
+                            "Address":Address1,"Contact":Contact_No1})    
 
         elif choice == "2":
-            Catogary2 = "Hostel"
             HN = input("Name of Hostel: ")
             Type2 = input("Type of Hostel: ")
             Stay_Fees_Per_Month = float(input(" Stay Fees Per Month: "))
@@ -40,66 +40,62 @@ while True:
             name2 = input("Name of Warden: ")
             Address2 = input("Address of Hostel: ")
             Contact_No2=input("For More Information Contact to This: ")
-    
-            details.append([ Catogary2,HN,Type2,Stay_Fees_Per_Month,Mess_Fees_Per_Month,Deposite2,Time2, Food_Menu2,Name2,name2,Address2,Contact_No2])    
-
+            details.append({"Category":"Hostel","Name":HN,"Type":Type2,"Fees":Stay_Fees_Per_Month,"fees":Mess_Fees_Per_Month,
+                            "Deposite":Deposite2,"Time":Time2,"Food":Food_Menu2,"Owner":Name2,"Warden":name2,
+                            "Address":Address2,"Contact":Contact_No2})  
         elif choice == "3":
-            Catogary3 = "Rental House/Flat"
             RHFN = input("Name of House/Flat/Area/Apartment: ")
             Type3 = input("Type : ")
             Rent_Per_Month3 = float(input("Rent Per Month: "))
-            Deposite3 = input("Deposite: ")1
-            
+            Deposite3 = input("Deposite: ")
             Time3 = input("Timings: ")
             Name3 = input("Name of Owner: ")
             Address3 = input("Address : ")
             Contact_No3 =input("For More Information Contact to This: ")
     
-            details.append([ Catogary3,RHFN,Type3,Rent_Per_Month3,Deposite3,Time3,Name3,Address3,Contact_No3])
+            details.append({"Category":"Rental House/Flat","Name":RHFN,"Type":Type3,"Rent":Rent_Per_Month3,
+                            "Deposite":Deposite3,"Time":Time3,"Owner":Name3,
+                            "Address":Address3,"Contact":Contact_No3})  
         
 
     elif choice == "2":
+         if len(details) == 0:
+             print("\n Welcome to gudsethu!")
+             print("No Details Available!")
+             continue
+         
+         print("\n Welcome to gudsethu!")
+        
+         print("1. PG")
+         print("2. Hostel")
+         print("3. Rental House/Flat")
 
-        for detail in details:
-            if detail[0]=="PG":
+         Category = input("Select Category: ")
 
-               print("\nCatogary:",detail[0])
-               print("Name:",detail[1])
-               print("Type:",detail[2])
-               print("Rent:",detail[3])
-               print("Deposite:",detail[4])
-               print("Time:",detail[5])
-               print("Food Menu:",detail[6])
-               print("Owner:",detail[7])
-               print("Address:",detail[8])
-               print("Contact:",detail[-1])
+         if Category == "1":
+            selected = "PG"
+         elif Category == "2":
+              selected = "Hostel"
+         elif Category == "3":
+              selected = "Rental House/Flat"
 
-            elif detail[0]=="Hostel":
+         filtered = []
 
-                 print("\nCatogary:",detail[0])
-                 print("Name:",detail[1])
-                 print("Type:",detail[2])
-                 print("Stay Fees Per Month:",detail[3])
-                 print("Mess Fees Per Month:",detail[34])
-                 print("Deposite:",detail[5])
-                 print("Time:",detail[6])
-                 print("Food Menu:",detail[7])
-                 print("Owner:",detail[8])
-                 print("warden:",detail[9])
-                 print("Address:",detail[10])
-                 print("Contact:",detail[-1])
+         print("\nAvailable:")
+         for item in details:
+            if item["Category"] == selected:
+               filtered.append(item)
+               print(f"{len(filtered)}. {item['Name']}")
 
-            elif detail[0]=="Rental House/Flat":
+         num = int(input("Select number: "))
 
-                 print("\nCatogary:",detail[0])
-                 print("Name:",detail[1])
-                 print("Type:",detail[2])
-                 print("Rent:",detail[3])
-                 print("Deposite:",detail[4])
-                 print("Time:",detail[5])
-                 print("Owner:",detail[6])
-                 print("Address:",detail[7])
-                 print("Contact:",detail[-1])
+         if 1 <= num <= len(filtered):
+            item = filtered[num - 1]
+
+            print("\n--- Details ---")
+            for key, value in item.items():
+                print(f"{key}: {value}")
+             
     elif choice == "3":
-        print("Thank You")
+        print("Thank You!")
         break
