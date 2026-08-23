cc1742734309d9efde963e840cf73fd2f5c54770
Shravya S V <sshaaru70@gmail.com>
2026-08-23 16:57:01 +0530

    Update gudsethu - Copy.py

--- gudsethu - Copy.py
+++ gudsethu - Copy.py
@@ -1,3 +1,4 @@
+print("---💐WELCOME TO gudsethu🌉🛖---")
 details = []
 
 while True:
@@ -11,6 +12,7 @@ while True:
         print("\n1.Paying Guest(pg)")
         print("2.Hostel")
         print("3.Rental House/Flat")
+        print("4.Hotel Room/Lodge")
         choice = input("Enter choice: ")
 
         if choice == "1":
@@ -46,7 +48,7 @@ while True:
                             "Deposite":Deposite2,"Time":Time2,"Food":Food_Menu2,"Owner":Name2,"Warden":name2,
                             "Address":Address2,"Area":Area2,"Contact":Contact_No2})  
         elif choice == "3":
-            RHFN = input("Name of House/Flat/Area/Apartment: ")
+            RHFN = input("Name of House/Flat/Apartment: ")
             Type3 = input("Type : ")
             Rent_Per_Month3 = float(input("Rent Per Month: "))
             Deposite3 = input("Deposite: ")
@@ -59,19 +61,26 @@ while True:
             details.append({"Category":"Rental House/Flat","Name":RHFN,"Type":Type3,"Rent":Rent_Per_Month3,
                             "Deposite":Deposite3,"Time":Time3,"Owner":Name3,
                             "Address":Address3,"Area":Area3,"Contact":Contact_No3})  
-        
-
+        elif choice == "4":
+            HR_L = input("Name of Hotel/Lodge: ")
+            Type4 = input("Type : ")
+            Price= float(input("Price Per day: "))
+            Advance_Payment = input("Advance_payment:")
+            Address4 = input("Address : ")
+            Area4 = input("Area:")
+            Contact_No4 =input("For More Information Contact to This: ")
+            
+            details.append({"Category":"Hotel/Lodge","Name":HR_L,"Type":Type4,"Price":Price,
+                                    "Advance":Advance_Payment,"Address":Address4,"Area":Area4,"Contact":Contact_No4})  
     elif choice == "2":
          if len(details) == 0:
-             print("\n Welcome to gudsethu!")
-             print("No Details Available!")
+             print("No Details Available!👎")
              continue
-         
-         print("\n Welcome to gudsethu!")
         
          print("1. PG")
          print("2. Hostel")
          print("3. Rental House/Flat")
+         print("4. Hotel/Lodge")
 
          Category = input("Select Category: ")
 
@@ -81,30 +90,48 @@ while True:
               selected = "Hostel"
          elif Category == "3":
               selected = "Rental House/Flat"
+         elif Category == "4":
+              selected = "Hotel/Lodge"
          else:
-             print("invalid Category!")
+             print("invalid Category!❌")
              continue
-         Area = input("Enter Area: ")
-
-         filtered = []
+        
+         Category_data = []
 
          for item in details:
-            if item["Category"] == selected and item["Area"].lower() == Area.lower() :
+            if item["Category"] == selected :
+               Category_data.append(item)
+         
+         if not Category_data:
+            print("\n Details are Not Available👎")
+            continue
+         
+         Area = input("Enter Area: ")
+         
+         filtered = []
+         for item in Category_data:
+            if item["Area"].lower() == Area.lower() :
                filtered.append(item)
-         if filtered:
-            print("\nAvailable:")
-            for i, item in enumerate(filtered, 1):
-                      
-                print(f"{len(filtered)}. {item['Name']}")
-            num = int(input("Select number: "))
-            if 1 <= num <= len(filtered):
+         if not filtered:
+            print("\n Details are Not Available👎")
+            continue
+         else:
+             print("\n Available👍:-")
+         
+         for i, item in enumerate(filtered, 1):      
+                print(f"{i}. {item['Name']}")
+         
+         num = int(input("Select number: "))
+         
+         if 1 <= num <= len(filtered):
                item = filtered[num - 1]
 
-               print("\n--- Details ---")
+               print("\n--- Details🫠 ---")
                for key, value in item.items():
                    print(f"{key}: {value}")
          else:
-                print("Details are not Available in this Area!")  
+                print("Details are not Available in this Area!😞")  
+    
     elif choice == "3":
-        print("Thank You!")
+        print("Thank You🙏!")
         break
